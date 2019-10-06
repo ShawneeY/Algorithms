6754442dc08f966da50cac40d4bb3ac100a57279
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-06 23:36:25 +1100

    N-ary iterative --ac

--- 589_N-nary_*.js
+++ 589_N-nary_*.js
@@ -1,3 +1,4 @@
+// Recursive
 var preorder = function(root) {
 
     var newList = [];
@@ -14,9 +15,42 @@ var preorder = function(root) {
             }
 
         }
-        
     }
     preorder(root);
+    return newList;
+    
+};
+
+//Iterative
+var preorder = function(root) {
+    if(root === null ){
+        return [];
+    }
+    var newList = [];
+    var stack = [];
+    var current = root;
+    while(current != null || stack.length > 0) {
+         while(current){
+            newList.push(current.val);
+             if(current.children.length > 0) {
+                 stack.push(current);
+             }
+            current = current.children[0];
+        }
+        
+        current = stack.pop();
+        if(current){
+            var subList = current;
+            subList.children.splice(0,1);
+            if(subList.children.length > 0){
+                stack.push(subList);
+            }
+
+            current = subList.children[0];
+        }
+      
+    
+    }
     return newList;
     
 };
