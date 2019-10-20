4b9903b9ad079009eedf82e7ca1c585aafc478b7
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-20 13:06:38 +1100

    Sorted array to binary search tree --nac take2

--- 108_convert_sorted_array_to_binary_search_tree.js
+++ 108_convert_sorted_array_to_binary_search_tree.js
@@ -3,30 +3,17 @@ var sortedArrayToBST = function(nums) {
     if(!nums){
         return null;
     }
-    const numsCount = nums.length;
-    if(numsCount === 0){
-        return null;
+    if(nums.length===1) {
+        return {val:nums[0], left:null, right:null}
     }
-    if(numsCount % 2 === 0){
-        return null;
-    }
-    var newTree = [];
-    var topNode = {val:nums[(numsCount -1)/2], left:{val:null}, right:{val:null}};
-
-    const secondHalf = nums.slice((numsCount +1)/2).reverse();
-    const firstHalf = nums.slice(0, (numsCount -1)/2);
-
-    for(var i=0; i<firstHalf.length; i++){
-        var left = firstHalf[i];
-        topNode = {val:left, left:{val:null}, right:{val:null}}
-        
-    }
-    for(var i=0; i<secondHalf.length; i++){
-        var right = secondHalf[i];
-        if(right){
-            newTree = {val:right, left:{val:null}, right:{val:null}}
-        }
-       
-    }
-    return newTree;
+    
+    
+    var count = nums.length;
+    var rootIndex = (count-1)/2;
+    
+     var newTree = {val:nums[rootIndex], left: null, right:null}
+    
+    newTree.left = sortedArrayToBST(nums.slice[0, rootIndex])
+    newTree.right = sortedArrayToBST(nums.slice[rootIndex+1])
+    return newTree
 };
