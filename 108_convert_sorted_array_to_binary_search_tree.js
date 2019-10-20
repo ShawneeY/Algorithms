7b88fa833ce1c393701a626ebd9ab39ec24f73b0
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-20 13:45:15 +1100

    Sorted array to binary search tree --ac

--- 108_convert_sorted_array_to_binary_search_tree.js
+++ 108_convert_sorted_array_to_binary_search_tree.js
@@ -1,19 +1,15 @@
-
 var sortedArrayToBST = function(nums) {
-    if(!nums){
+    if(!nums || nums.length === 0){
         return null;
     }
-    if(nums.length===1) {
-        return {val:nums[0], left:null, right:null}
-    }
-    
-    
+  
     var count = nums.length;
-    var rootIndex = (count-1)/2;
+    var rootIndex = Math.floor(count/2);
     
-     var newTree = {val:nums[rootIndex], left: null, right:null}
+    var newTree = {val:nums[rootIndex], left: null, right:null}
     
-    newTree.left = sortedArrayToBST(nums.slice[0, rootIndex])
-    newTree.right = sortedArrayToBST(nums.slice[rootIndex+1])
+    newTree.left = sortedArrayToBST(nums.slice(0, rootIndex))
+    newTree.right = sortedArrayToBST(nums.slice(rootIndex+1))
+    
     return newTree
 };
