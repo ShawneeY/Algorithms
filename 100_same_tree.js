8af14b8082a597bdf0f6235bf8222ff04cce43d9
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-20 16:05:59 +1100

    check if same tree --ac

--- 100_same_tree.js
+++ 100_same_tree.js
@@ -1,9 +1,15 @@
 var isSameTree = function(p, q) {
-    if(!p || !q ){
-        return p === q;
-    }
-    isSameTree(p.left, q.left);
+    if (!p && !q) {
+         return true;
+     }
+     if (!p || !q) {
+         return false;
+     }
+     if(p.val !== q.val) {
+       isSame =false;
+     }
+       isSameTree(p.left, q.left);
     isSameTree(p.right, q.right);
-    
-   return p === q;
+   
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
