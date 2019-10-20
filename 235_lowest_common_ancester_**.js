388a811a1f5f0e3cd0ab66429d478a4b15f9d6b4
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-21 00:02:57 +1100

    235 lowest ancester --ac

---
+++ 235_lowest_common_ancester_**.js
@@ -0,0 +1,17 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if(root === null){
+        return null;
+    }
+   
+    if(p.val< root.val && q.val<root.val){
+        return lowestCommonAncestor(root.left, p, q);
+    }
+    
+    if(p.val > root.val && q.val > root.val){
+        return lowestCommonAncestor(root.right, p, q);
+    }
+    
+    else {
+        return root;
+    }
+};
