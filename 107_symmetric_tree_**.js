9ce0af5f376970ccea2cc2143c778c5978ccb631
Shawnee Yu <shawnee.yu@rea-group.com>
2019-10-21 21:47:09 +1100

    symmetric tree allans version --ac

---
+++ 107_symmetric_tree_**.js
@@ -0,0 +1,19 @@
+var levelOrderBottom = function(root) {
+    // base case
+    
+    var levels = [];
+    var levelTraversal = function(root, level){
+      if(root === null){
+          return;
+      }
+       if(!levels[level]) {
+           levels[level] = [];
+       }
+      levels[level].push(root.val);
+        levelTraversal(root.left, level+1);
+        levelTraversal(root.right, level+1);
+          
+    }
+    levelTraversal(root,0);
+    return levels.reverse();
+};
