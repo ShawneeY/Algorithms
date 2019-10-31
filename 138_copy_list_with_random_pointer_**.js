f43e201998fee0ba1a1e90fcec2decb2c489bd9d
Shawnee Yu <shawnee.yu@rea-group.com>
2019-11-01 00:10:53 +1100

    138 copy list with random pointer --ac

--- 138_copy_list_with_random_pointer_**.js
+++ 138_copy_list_with_random_pointer_**.js
@@ -1,3 +1,4 @@
+//nac
 var copyRandomList = function(head) {
     var copyHead = {val: null, next:null, random:null}
     var fakeList = copyHead;
@@ -10,4 +11,23 @@ var copyRandomList = function(head) {
         fakeList = fakeList.next;
     }
     return copyHead.next;
+};
+
+//ac
+var copyRandomList = function(head) {
+    const map = new Map();
+ 
+    function copy(node)   {
+    if (node == null) return null;
+    if (map.get(node) != null) return map.get(node);
+
+    const n = new Node(node.val);
+    map.set(node, n);
+
+    n.next = copy(node.next);
+    n.random = copy(node.random);
+    return n;
+    }
+
+    return copy(head);
 };
