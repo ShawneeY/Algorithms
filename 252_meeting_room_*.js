5303a3fc0656f0ebe2af530e9c6a87e49d4a8d16
Shawnee Yu <shawnee.yu@rea-group.com>
2019-11-04 15:50:36 +1100

    252 meeting rooms better solution -ac

--- 252_meeting_room_*.js
+++ 252_meeting_room_*.js
@@ -22,3 +22,29 @@ var canAttendMeetings = function(intervals) {
     return true;
     
 };
+
+// better one
+var canAttendMeetings = function(intervals) {
+    
+    if(intervals.length === 0){
+        return true;
+    }
+    
+    function isOverlapping(a, b){
+        return Math.max(b[0], a[0]) < Math.min(b[1], a[1]);
+    }
+    
+    intervals.sort(function(a,b){return a[0] - b[0]});
+    
+    for(var i=0; i<intervals.length-1; i++){
+        var sl = intervals[i+1][0];
+        var el = intervals[i][1]
+        
+        if(sl < el){
+            return false;
+        }
+    }
+    return true;
+    
+};
+//O(n) + O(nlgn)
