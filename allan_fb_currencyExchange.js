b218ea5249f2dc5cd7c0a82db6330c49058d6587
Shawnee Yu <shawnee.yu@rea-group.com>
2019-12-01 14:02:42 +1100

    FB real quiz --ac

--- allan_fb_currencyExchange.js
+++ allan_fb_currencyExchange.js
@@ -19,49 +19,58 @@ Results:
 
 */
 
-var canConvert = function(rates, queries) {
-    var map = new Map();
-   
-    var constructMap = function(map, from, to){
-       if(map.get(from)){
-            var toMap = map.get(from);
-            if(!toMap.get(to)){
-                toMap.set(to, true);
-            }
-        }else{
-            var toMap = new Map();
-            toMap.set(to, true);
-            map.set(from, toMap);
-        }
-    }
-        
-  
-    for(var i = 0; i < rates.length; i++){
-        var from = rates[i][0];
-        var to = rates[i][1];
-        constructMap(map, from, to);
-        constructMap(map, to, from);
-    }
-
-    var results=[];
-    
-    for(var i = 0; i < queries.length; i++){
-        var from = queries[i][0];
-        var to = queries[i][1];
-        
-        if(map.get(from)){
-            var toMap = map.get(from);
-            if(toMap.get(to)){
-                results.push(true);
-            }else{
-                results.push(false);
-            }
-        }else{
-            results.push(false);
-        }
-    }
-    return results;
+var constructGraph = function(graph, from, to){
+    if(graph.get(from)){
+          var currency = graph.get(from);
+          currency.add(to);
+      }else {
+          var toSet = new Set();
+          toSet.add(to);
+          graph.set(from, toSet);
+          
+      }
 }
 
-var results = canConvert([["USD", "EUR"], ["EUR", "JPY"], ["USD", "JPY"]], [["USD", "JPY"],["JPY", "EUR"],["USD", "CAD"]])
-console.log(results)
+
+
+var canConvert = function(rates, a, b) {
+  // adjacency list
+  var graph = new Map()
+  
+  
+  for(var i = 0; i < rates.length; i++ ){
+      var from = rates[i][0];
+      var to = rates[i][1];
+      constructGraph(graph, from, to);
+      constructGraph(graph, to, from);  
+  }
+  
+  let queue = [a];
+  let visited = new Set();
+  while(queue.length){
+      const length = queue.length;
+      for(let i = 0; i < length; i++){
+          const curr = queue.shift();
+          if(visited.has(curr)) continue; 
+          
+          if(curr == b) return true;
+          
+          visited.add(curr);
+          
+          const neighbours = graph.get(curr);
+          
+          for(let neighbour of neighbours){
+              queue.push(neighbour);
+          }
+      }
+
+  }
+  return false; 
+}
+  
+var rates = [["USD", "EUR"], ["EUR", "JPY"], ["USD", "JPY"]];
+var a = "USD";
+var b = "CAD";
+
+var result = canConvert(rates, a, b);
+console.log(result);
