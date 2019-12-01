1b8b8419082daeb044d5cc70dcf1a0d0bdcd77cc
Shawnee Yu <shawnee.yu@rea-group.com>
2019-12-01 14:10:18 +1100

    FB real quiz better version

--- allan_fb_currencyExchange.js
+++ allan_fb_currencyExchange.js
@@ -74,3 +74,81 @@ var b = "CAD";
 
 var result = canConvert(rates, a, b);
 console.log(result);
+
+// Better version
+// https://aonecode.com/facebook-interview-questions-currency-exchange
+
+/*
+
+/*
+
+Given a list of currency exchange table, determine if currency A can be converted to currency B
+
+example:
+
+Exchange table:
+[["USD", "EUR"], ["EUR", "JPY"], ["USD", "JPY"]]
+
+Queries:
+[["USD", "JPY"],["JPY", "EUR"],["USD", "CAD"]]
+
+Results:
+[true, true, false]
+
+*/
+
+var constructGraph = function(graph, from, to){
+    if(graph.get(from)){
+          var currency = graph.get(from);
+          currency.add(to);
+      }else {
+          var toSet = new Set();
+          toSet.add(to);
+          graph.set(from, toSet);
+          
+      }
+}
+
+
+
+var canConvert = function(rates, a, b) {
+  // adjacency list
+  var graph = new Map()
+  
+  
+  for(var i = 0; i < rates.length; i++ ){
+      var from = rates[i][0];
+      var to = rates[i][1];
+      constructGraph(graph, from, to);
+      constructGraph(graph, to, from);  
+  }
+  
+  let queue = [a];
+  let visited = new Set();
+ visited.add(a);
+  while(queue.length){
+      const length = queue.length;
+      for(let i = 0; i < length; i++){
+          const curr = queue.shift();
+        
+          if(curr == b) return true;
+          
+          const neighbours = graph.get(curr);
+          
+          for(let neighbour of neighbours){
+              if(visited.has(neighbour)) continue;
+              queue.push(neighbour);
+              visited.add(neighbour);
+          }
+      }
+
+  }
+  return false; 
+}
+  
+var rates = [["USD", "EUR"], ["EUR", "JPY"], ["USD", "JPY"]];
+var a = "USD";
+var b = "CAD";
+
+var result = canConvert(rates, a, b);
+console.log(result);
