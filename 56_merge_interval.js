f8e40347ebc85ccca7397237dcf7744c4be1ebc3
Shawnee Yu <shawnee.yu@rea-group.com>
2019-12-10 22:13:37 +1100

    56 --ac 完全会做

--- 56_merge_interval.js
+++ 56_merge_interval.js
@@ -21,3 +21,30 @@ var merge = function(intervals) {
     return results;
     
 };
+
+// Second go
+
+var merge = function(intervals) {
+    if(intervals.length === 0){
+        return [];
+    }
+    let sorted = intervals.sort(function(a,b){return a[0] - b[0]});
+    
+    let mergedList = [sorted[0]];
+    
+    for(let i = 0; i < sorted.length; i++){
+        let length = mergedList.length;
+        
+        let mergedItem = mergedList[length - 1];
+        
+        let curr = sorted[i];
+
+        if ((mergedItem[1] >= curr[0] && mergedItem[1] <= curr[1]) || mergedItem[1] > curr[1]){
+            mergedList.pop();
+            mergedList.push([Math.min(curr[0], mergedItem[0]), Math.max(curr[1], mergedItem[1])]);
+        }else {
+            mergedList.push(curr);
+        }
+    }
+    return mergedList;
+};
