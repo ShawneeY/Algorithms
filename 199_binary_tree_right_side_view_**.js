2660f4ea2a9a4b7901b56a4343283fb305a005f1
Shawnee Yu <shawnee.yu@rea-group.com>
2019-12-22 23:52:34 +1100

    199 bfs --ac

--- 199_binary_tree_right_side_view_**.js
+++ 199_binary_tree_right_side_view_**.js
@@ -20,3 +20,37 @@ var rightSideView = function(root) {
     helper(root, 0);
     return right;
 };
+
+//bfs
+
+var rightSideView = function(root) {
+    
+    if(root == null){
+        return [];
+    }
+    
+    let que = [root];
+    let lvl = 0;
+    
+    let right = [];
+    while(que.length > 0){
+        let length = que.length;
+        let layerVal = [];
+        for(let i = 0; i < length; i++){
+            let curr = que.shift();
+            
+            if(curr){
+                layerVal.push(curr.val);
+            }
+            if(curr.left){
+                que.push(curr.left);
+            }
+            if(curr.right){
+                que.push(curr.right);
+            }
+        }
+        console.log(layerVal)
+        right.push(layerVal.pop());
+    }
+    return right;
+};
