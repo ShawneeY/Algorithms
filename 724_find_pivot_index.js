9242dbe7f75d8be747dde9344ebee15f9bb1b1be
Shawnee Yu <shawnee.yu@rea-group.com>
2019-12-28 17:55:21 +1100

    724 running sum solution  --ac

--- 724_find_pivot_index.js
+++ 724_find_pivot_index.js
@@ -21,3 +21,31 @@ var pivotIndex = function(nums) {
    }
     return result;
 };
+
+//Better solution
+var pivotIndex = function(nums) {
+    if(nums.length == 0){
+        return -1;
+    }
+    
+
+    let sum = 0;
+    let runningSum = [];
+    //runningSum = [1,8,11,17,22,28];
+    
+    for(let i = 0; i < nums.length; i++){
+        sum = nums[i] + sum;
+        runningSum[i] = sum;
+    }
+
+    for(let i = 0; i < nums.length; i++){
+        let leftSum = i === 0 ? 0 : runningSum[i-1];
+        let rightSum = i === nums.length - 1 ? 0 : runningSum[nums.length-1] - runningSum[i];
+       
+        if(leftSum === rightSum){
+            return i;
+        }
+    }
+    return -1;
+
+}
