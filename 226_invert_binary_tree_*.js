400e184c62ba0546d22700f783c2c9d9469865b3
Allan Gong <gonghany@amazon.com>
2020-01-23 21:45:25 -0800

    Remove filenames with *

--- 226_invert_binary_tree_*.js
+++
@@ -1,9 +0,0 @@
-var invertTree = function(root) {
-    if (root === null || root === []) {
-         return root;
-    }
-    var left = root.left;
-    root.left = invertTree(root.right);
-    root.right = invertTree(left);
-    return root;
-};
--- 237_delete_node _in_a_linked_list_*.js
+++
@@ -1,9 +0,0 @@
-
-var deleteNode = function(node) {
-    if(!node.next){
-        node = null;
-    }
-   node.val = node.next.val;
-   node.next = node.next.next;
-   
-};
