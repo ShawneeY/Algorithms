ade2a015da98639435f03908e1cdd6d289dd994e
ShawneeY <shawyew@gmail.com>
2020-01-24 23:58:32 -0800

    updated 5 faster --ac

--- 5_longest_palindromic_substring.js
+++ 5_longest_palindromic_substring.js
@@ -41,3 +41,46 @@ var longestPalindrome = function(s) {
     
     return maxArray.join('');
 };
+
+// Updated version
+
+var longestPalindrome = function(s) {
+    
+    let palindromicMap = new Map();
+    let L,R;
+    
+    let findPalindromicSubstring = function(left,right){
+
+        if(left>=0 && right <s.length && s[left] === s[right]){
+            findPalindromicSubstring(left-1, right+1);
+        } else{
+            L = left+1;
+            R = right;
+        }
+    }
+
+    let maxLength = 0;
+    let maxArray = '';
+    let sameCharArr = [];
+
+    for(let i = 0; i < s.length; i++){
+        if(i<s.length-1 && s[i] === s[i+1]){
+            findPalindromicSubstring(i, i+1);
+            let array = s.slice(L, R);
+            if (array.length > maxLength) {
+                maxArray = array;
+                maxLength = array.length;
+            }
+        }
+       
+        findPalindromicSubstring(i, i);
+
+        let array = s.slice(L, R);
+        if (array.length > maxLength) {
+            maxArray = array;
+            maxLength = array.length;
+        }
+    }
+    
+    return maxArray;
+};
