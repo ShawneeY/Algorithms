ceb14096a5f878f96aae5bfa9b373bc1d5a47589
ShawneeY <shawyew@gmail.com>
2020-01-27 15:59:56 -0800

    added explaination to 3

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -19,10 +19,10 @@ var lengthOfLongestSubstring = function(s) {
 
     for(let i = 0; i < s.length; i++){
         length = Math.max(i - p1, length)
-        if(map.get(s[i]) >= p1){
+        if(map.get(s[i]) >= p1){ // if repeat found, get the length before the repetitive one
             let dupeInMapIndex = map.get(s[i]);
             p1 = dupeInMapIndex +1;
-        }else {
+        }else { // if repeat not found, include current element in the length
             length = Math.max(i - p1+1, length)
         }
         map.set(s[i], i)
