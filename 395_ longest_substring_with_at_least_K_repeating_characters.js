18b8ac228748191ca2adf79982bdc8bff7b964df
ShawneeY <shawyew@gmail.com>
2020-01-30 11:52:27 -0800

    Shawnee solution -ac 395

--- 395_ longest_substring_with_at_least_K_repeating_characters.js
+++ 395_ longest_substring_with_at_least_K_repeating_characters.js
@@ -55,3 +55,59 @@ var longestSubstringWithNUniqueCharacters = function(s, k, n) {
     
     return maxLength;
 }
+
+// Shawnee's solution
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestSubstring = function(s, k) {
+    let maxLength = 0;
+    for(let j = 1; j <= 26; j++){
+        let smap = new Map();
+        let uniqueChar = 0;
+        let slow = 0;
+        let validSubstring = 0 
+        ;
+        for(let fast = 0; fast < s.length; fast++){
+            if(smap.get(s[fast]) > 0){
+                 let fastCount = smap.get(s[fast]);
+                 smap.set(s[fast], fastCount+1);
+            }else{
+                 smap.set(s[fast], 1);
+                 uniqueChar++;
+            }
+
+            let count = smap.get(s[fast]);
+            if(count === k){
+                validSubstring++
+            }
+           
+            while(uniqueChar > j){
+                
+                let slowCount = smap.get(s.charAt(slow));
+                smap.set(s.charAt(slow), slowCount - 1);
+                if(slowCount === k){
+                    validSubstring--
+                }
+                if(slowCount === 1){
+                    uniqueChar--
+                }
+                slow++
+
+                
+            }
+
+            if(uniqueChar === j && uniqueChar === validSubstring){ 
+                console.log(uniqueChar, fast, slow)
+             let length = fast - slow + 1;
+             maxLength = Math.max(length, maxLength);                
+            }
+
+        }
+    }
+    
+    return maxLength;
+   
+};
