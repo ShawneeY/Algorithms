2cc04e4039646546b9c8e129aa0bfab653978aa3
ShawneeY <shawyew@gmail.com>
2020-02-03 13:17:05 -0800

    104 二刷 --ac

--- 104_maximum_depth_of_binary_tree.js
+++ 104_maximum_depth_of_binary_tree.js
@@ -6,3 +6,18 @@ var maxDepth = function(root) {
   return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1
 };
 // Tail recursion
+
+
+//二刷
+var maxDepth = function(root) {
+  // edge case
+  
+  if(root === null){
+      return 0;
+  }
+  
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  
+  return Math.max(left, right) + 1;
+};
