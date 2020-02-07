941e7e53629c764a8d5d084bf68becd6b3b246a2
ShawneeY <shawyew@gmail.com>
2020-02-06 16:49:51 -0800

    21 second go --ac

--- 21_merge_sorted_lists_.js
+++ 21_merge_sorted_lists_.js
@@ -15,3 +15,32 @@ var mergeTwoLists = function(l1, l2) {
   
   return mergedHead.next;
 };
+
+// second go 
+var mergeTwoLists = function(l1, l2) {
+   
+    
+    let root = null;
+    let recursion = (l1, l2, node) => {
+        if(!l1 && !l2){
+            return null;
+        }
+         if(l1 === null || l2 === null){
+           return node = l1 ? l1 : l2;
+         }
+        
+        if(l1.val >= l2.val){
+            node = {val:l2.val}
+            node.next =  recursion(l1, l2.next, node);
+        } else {
+            node = {val:l1.val}
+            node.next =  recursion(l1.next, l2, node);
+        }
+    
+        
+        return node;
+    }
+   
+   return recursion(l1, l2, root);
+  
+};
