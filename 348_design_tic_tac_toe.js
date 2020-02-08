fefef4f761b6e3e543e3c9bae09f9ee038cb6e0d
ShawneeY <shawyew@gmail.com>
2020-02-07 22:35:50 -0800

    348 0(1)  --ac

--- 348_design_tic_tac_toe.js
+++ 348_design_tic_tac_toe.js
@@ -89,3 +89,52 @@ TicTacToe.prototype.move = function(row, col, player) {
  * var obj = new TicTacToe(n)
  * var param_1 = obj.move(row,col,player)
  */
+
+ //o(1)
+ /**
+ * Initialize your data structure here.
+ * @param {number} n
+ */
+var TicTacToe = function(n) {
+    this.n = n;
+    this.row = [];
+    this.col = [];
+    this.diagonal1 = 0;
+    this.diagonal2 = 0;
+};
+
+/**
+ * Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. 
+ * @param {number} row 
+ * @param {number} col 
+ * @param {number} player
+ * @return {number}
+ */
+TicTacToe.prototype.move = function(row, col, player) {
+    let p = player === 1 ? 1 :-1;
+   
+    let n = this.n;
+    
+    this.row[row] = this.row[row] ? this.row[row]+p : p;
+    this.col[col] = this.col[col] ? this.col[col]+p : p;
+    if(row === col) this.diagonal1 = this.diagonal1+p; 
+    if(row+col === n-1) this.diagonal2 = this.diagonal2+p;
+    if(Math.abs(this.row[row]) === n || Math.abs(this.col[col]) === n || Math.abs(this.diagonal1) === n || Math.abs(this.diagonal2) === n){
+        
+        return player;
+    }
+    return 0;
+};
+
+/** 
+ * Your TicTacToe object will be instantiated and called as such:
+ * var obj = new TicTacToe(n)
+ * var param_1 = obj.move(row,col,player)
+ */
