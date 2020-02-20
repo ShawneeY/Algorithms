b3949815f029ac3f377b23ca2f8ad02e09e941d2
ShawneeY <shawyew@gmail.com>
2020-02-20 14:39:51 -0800

    4 --hard

---
+++ 4_Median_of_Two_Sorted_Arrays.js
@@ -0,0 +1,14 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    const combined = [...nums1, ...nums2];
+    
+    combined.sort(((a,b) =>  a-b));
+   
+    const length = combined.length;
+    if(length%2 !== 0){
+        return combined[Math.floor(length/2)];
+    }else{
+        const n1 = combined[length/2 -1];
+        const n2 = combined[length/2];
+        return (n1+n2)/2
+    }
+};
