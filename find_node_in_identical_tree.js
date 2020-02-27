246accc4169789bacc043249b7b0ac13042ae793
Allan Gong <hanyun.gong@gmail.com>
2020-02-27 13:46:49 -0800

    Update find_node_in_identical_tree.js

--- find_node_in_identical_tree.js
+++ find_node_in_identical_tree.js
@@ -1,10 +1,12 @@
 var findNode = function(tree1, node1, tree2) {
+    // base case
     if(tree1 === null || tree2 === null){
-          return;
+          return null;
     }
 
     if(tree1 === node1){
         return tree2;
     }
-    return findLvl(tree1.left, tree2.left, lvl+1) || findLvl(tree1.right,tree2.right, lvl+1);
+    
+    return findLvl(tree1.left, node1, tree2.left) || findLvl(tree1.right, node1, tree2.right);
 };
