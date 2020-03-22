a1d9dae09df7335763a9feb509473fa88857c477
ShawneeY <shawyew@gmail.com>
2020-03-21 22:35:47 -0700

    tidied up roman to interger

--- 13_roman_to_integer_.js
+++ 13_roman_to_integer_.js
@@ -1,95 +1,41 @@
-//Option 1
-const romanValue = {
-    I: 1,
-    V: 5, 
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-}
-
-const substractionTable = {
-    IV: 4,
-    IX: 9,
-    XL: 40,
-    XC: 90,
-    CD: 400,
-    CM: 900
-}
-const substractionSubStr = [
-    "IV", "IX", "XL", "XC", "CD", "CM"
-]
-let subStrs = [];
-var getSubstractionSubStr = function(str){
-    subStrs = [];
-    substractionSubStr.forEach(function(subStr){
-        if(str.includes(subStr)){
-           subStrs.push(subStr);   
-        }
-    });
-}
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var romanToInt = function(s) {
-    let numericValue=0;
-    getSubstractionSubStr(s);
+    const map = {
+        'I':1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
     
-    if(subStrs.length > 0) {
-        subStrs.forEach(function(substr){
-            numericValue += substractionTable[substr];
-            s = s.replace(substr,"");
-        })
-    }
-    if(s.length > 0) {
-          s.split("").map(function(item){
-            numericValue = numericValue + romanValue[item];
-        })
-    }
-    return numericValue;
-};
-
-//Option 2
-var romanToInt = function(s) {
-   let numericValue=0;
-  
-   return s.split("").reduce(function(accumulator, currentValue, currentIndex){
-        if(romanValue[currentValue] < romanValue[s[currentIndex + 1]] ){
-            numericValue -= romanValue[currentValue];
+    //IV:4 IX:9
+    //XL:40 XC:90
+    //CD:400 CM: 900
+    
+    const arr = s.split('');
+    let output = 0;
+    for(let i = 0; i < arr.length; i++){
+        let curr = arr[i];
+        let next = arr[i+1];
+        if(!next){
+            output = output + map[curr];
+            break;
+        } 
+        
+        let currVal = map[curr];
+        let nextVal = map[next];
+        
+        if(currVal < nextVal){
+            output = output-currVal
         }else {
-            numericValue += romanValue[currentValue];
+            output = output+currVal
         }
-     return numericValue;
-   }, 0 )
+        
+    }
+    return output;
 };
-
-// option 3
-public static int romanToInt(String s) {
-	int res = 0;
-	for (int i = s.length() - 1; i >= 0; i--) {
-		char c = s.charAt(i);
-		switch (c) {
-		case 'I':
-			res += (res >= 5 ? -1 : 1);
-			break;
-		case 'V':
-			res += 5;
-			break;
-		case 'X':
-			res += 10 * (res >= 50 ? -1 : 1);
-			break;
-		case 'L':
-			res += 50;
-			break;
-		case 'C':
-			res += 100 * (res >= 500 ? -1 : 1);
-			break;
-		case 'D':
-			res += 500;
-			break;
-		case 'M':
-			res += 1000;
-			break;
-		}
-	}
-	return res;
-}
