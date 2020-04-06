c6d179dbc44be61d7ff72fc2e60d13ac24bd8f60
ShawneeY <shawyew@gmail.com>
2020-04-05 23:05:39 -0700

    updated 398 to use reservoir sampling

--- 398_Random_Pick_Index.js
+++ 398_Random_Pick_Index.js
@@ -35,3 +35,32 @@ Solution.prototype.pick = function (target) {
  * var obj = new Solution(nums)
  * var param_1 = obj.pick(target)
  */
+
+/**
+* @param {number[]} nums
+*/
+var Solution = function (nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} target
+ * @return {number}
+ */
+Solution.prototype.pick = function (target) {
+    var result = 0;
+    var count = 0;
+    for (var i = 0; i < this.nums.length; i++) {
+        if (this.nums[i] == target) {
+            if (Math.floor(Math.random() * (++count)) == 0) result = i;
+
+        }
+    }
+    return result;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
